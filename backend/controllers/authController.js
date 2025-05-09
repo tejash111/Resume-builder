2e0f0e47d31730996cea80e0bf878eb61c209246
tejash111 <tejashsinghrajput@gmail.com>
2025-05-09 07:25:27 +0100

    added auth apis

---
+++ backend/controllers/authController.js
@@ -0,0 +1,95 @@
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+//genrate jwt token
+const genrateToken=(userId) => {
+    return jwt.sign({id:userId} , process.env.JWT_SECRET, {expiresIn: "7d"});
+}
+
+//@desc resgister a new user
+//@route POST /api/auth/register
+//@access Public
+const registerUser = async (req,res) => {
+    try {
+        const {name,email,password,porfileImageUrl}=req.body;
+
+        //check if user alredy exists
+        const userExists = await User.findOne({email});
+        if (userExists){
+            return res.status(400).json({message:"user already exist"});
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        //create new user
+        const user=await User.create({
+            name,
+            email,
+            password:hashedPassword,
+            porfileImageUrl,
+        });
+
+        //return user data with jwt
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            porfileImageUrl:user.profileImageUrl,
+            token:genrateToken(user._id),
+        });
+    } catch (error){
+        res.status(500).json({message: "server error",error:error.message})
+    }
+}
+
+//@desc Login user
+//@route POST /api/auth/login
+//@access Public
+const loginUser = async (req,res) => {
+    try{
+        const {email,password} = req.body;
+
+        const user = await User.findOne({email});
+        if (!user){
+            return res.status(500).json({message:"Invalid email or passoword"});
+        }
+
+        //compare passoword
+        const isMatch = await bcrypt.compare(password,user.password);
+        if (!isMatch) {
+            return res.status(500).json({message:"Invalid email or password"});
+        }
+
+        //Return user data with jwt
+        res.json({
+            _id: user._id,
+            name :user.name,
+            email: user.email,
+            profileImageUrl:user.profileImageUrl,
+            token:genrateToken(user._id),
+        })
+    } catch (error){
+        res.status(500).json({message: "server error",error:error.message})
+    }
+}
+
+//@desc Get user profile
+//@route GET /api/auth/profile
+// @access Private (Requires Jwt)
+
+const getUserProfile = async (req,res)=>{
+    try{
+        const user = await User.findById(req.user.id).select("-password");
+        if (!user){
+            return res.status(404).json({message : "user not found"})
+        }
+        res.json(user);
+    } catch (error){
+        res.status(500).json({message: "server error",error:error.message})
+    }
+}
+
+module.exports={registerUser,loginUser,getUserProfile}
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const express = require("express")
 const cors = require("cors")
 const path = require("path");
 const connectDB = require("./config/db");
+const resume = require("./models/resume");
+
+const authRoutes= require("./routes/authRoutes")
 
 const app = express();
 
@@ -20,6 +23,21 @@ connectDB();
 
 //middleware
 app.use(express.json());
+//app.use("/api/resume",resumeRoutes)
+
+//routes
+app.use("/api/auth",authRoutes)
+//app.use("/api/resume",)
+
+//serve upload folder
+app.use(
+    "/uploads",
+    express.static(path.join(__dirname,"uploads"),{
+        setHeaders: (res,path) => {
+            res.set("Acess-Control-Allow-Origin", "http://localhost:5173");
+        },
+    })
+);
 
 //start server
 const PORT = process.env.PORT || 5000;
