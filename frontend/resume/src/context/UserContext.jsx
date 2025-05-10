92326f9388ec95690032a8a992020e751f654d21
tejash111 <tejashsinghrajput@gmail.com>
2025-05-10 10:14:37 +0100

    creating axiosinstance & user-context-provider

--- frontend/resume/src/context/UserContext.jsx
+++ frontend/resume/src/context/UserContext.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React , {createContext,useState,useEffect} from "react";
+import axiosInstance from "../utils/axiosInstance";
+import { API_PATHS } from "../utils/ApiPaths";
 
-const UserContext = () => {
+export const UserContext=createContext();
+
+const UserProvider = ({children}) => {
+ 
+  const [user,setUser]=useState(null);
+  const [loading,setLoading] = useState(true); //new state to track loading
+
+    useEffect(() => {
+      if (user) return;
+
+      const accessToken = localStorage.getItem("token");
+      if (!accessToken) {
+        setLoading(false);
+        return;
+      }
+
+      const fetchUser = async () => {
+        try {
+          const response = await axiosInstance.get(API_PATHS.AUTH.GET_PROFILE);
+          setUser(response.data);
+        }catch (error) {
+          console.error("User not authenticated",error);
+          clearUser();
+        }finally {
+          setLoading(false);
+        }
+      };
+      fetchUser();
+    },[])
+
+    const updateUser =  (userData) => {
+      setUser(userData);
+      localStorage.setItem("token",userData.token); //save token
+      setLoading(false);
+    }
+
+    const clearUser=()=> {
+      setUser(null);
+      localStorage.removeItem("token");
+    };
   return (
-    <div>UserContext</div>
+    <UserContext.Provider value={{user,loading,updateUser,clearUser}}>
+      {children}
+    </UserContext.Provider>
   )
-}
+};
 
-export default UserContext
+export default UserProvider;
---
+++ frontend/resume/src/utils/axiosInstance.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { BASE_URL } from './ApiPaths';
+
+const axiosInstance = axios.create({
+    baseURL:BASE_URL,
+    timeout:10000,
+    headers: {
+        "Content-Type" : "application/json",
+        Accept: "application/json",
+    },
+});
+
+//request Interceptor
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem('token');
+
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+//Response Interceptor
+axiosInstance.interceptors.response.use(
+    (response)=> {
+        return response;
+    },
+    (error) => {
+        //handle common error globally
+        if (error.response) {
+            if (error.response.status===401){
+                //Redirect to login page
+                window.location.href= "/";
+            }else if (error.response.status === 500){
+                console.error("server error pls try again later.");
+            }
+        }else if (error.code === 'ECONNABORTED'){
+            console.log("Request timeout. Please try again.");
+            
+        }
+        return Promise.reject(error);
+    }
+);
+
+    export default axiosInstance
